7a5ed78cff27265d9433674163ef677626f7eb25
Pozzame <pozzame@gmail.com>
2024-07-29 11:06:59 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rng = new Random();
+
+Console.Clear();
+Console.WriteLine("-----Indovina PIN di 4 cifre-----");
+
+string PINSegreto = GeneraPIN();
+do
+{
+    Console.WriteLine("Inserisci PIN");
+} while(!ProvaPIN(Console.ReadLine()!));
+Console.WriteLine("Bravo");
+
+bool ProvaPIN(string PIN)
+{
+    if (PIN == PINSegreto)
+        return true;
+    else
+        return false;
+}
+
+string GeneraPIN()
+{
+    return rng.Next(1, 1000).ToString("D4");
+}
